7e987de00561181ba4fdcf8714c99fdff6a51949
yunweb <3358621@qq.com>
2017-08-17 22:49:30 +0800

    注释

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+      <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    data () {
+      return {
+        msg: '这个是头部'
+      }
+    }
+  }
+</script>
