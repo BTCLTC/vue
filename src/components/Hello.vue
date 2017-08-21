2509fad98c578bb4405a17f7a610ab8ee0e0563d
yunweb <3358621@qq.com>
2017-08-21 13:30:58 +0800

    新增scss插件

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="text">{{ msg }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'hello',
-  data () {
+  data() {
     return {
-      msg: '这个是身体'
+      msg: 'Welcome to Your Vue.js App'
     }
-  },
-  template: ''
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss">
 h1, h2 {
   font-weight: normal;
 }
@@ -35,4 +34,20 @@ li {
 a {
   color: #42b983;
 }
+.hello {
+  .text {
+    text-align: center;
+    background-image: -webkit-linear-gradient(left, #0acbe9, #ff0000 25%, #ff2ee1 50%, #fffc00 75%, #11eb97);
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    -webkit-background-size: 200% 100%;
+    -webkit-animation: masked-animation 4s infinite linear;
+  }
+
+  @-webkit-keyframes masked-animation {
+      0%{ background-position: 0 0;}
+      100% { background-position: -100% 0;}
+  }
+}
+
 </style>
